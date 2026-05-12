d108931dbc16a7f489300e3368376688c85921f3
김재웅 <grbuguj@users.noreply.github.com>
2026-05-12 20:34:52 +0900

    Init

---
+++ src/main/java/com/example/aifinanceworld/AiFinanceWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.aifinanceworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFinanceWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiFinanceWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aifinanceworld/AiFinanceWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aifinanceworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFinanceWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
